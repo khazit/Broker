521dbbd481a9d670605ac9752fa6b00eda95d4d0
Ali Benkassou <a.benkassou1@gmail.com>
2020-04-08 20:37:27 +0200

    Add setup.py to install broker module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Broker',
+    version='0.1.0',
+    url='https://github.com/khazit/Broker',
+    author='Ali Benkassou',
+    author_email='a.benkassou1@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),    
+)
