e30308b78e1f385ab66264c2b9dc5dc92323c726
Ali Benkassou <a.benkassou1@gmail.com>
2020-04-09 23:32:53 +0200

    Migrate to Flask

--- server.py
+++ server.py
@@ -1,28 +1,33 @@
 """Server side"""
 
-
 import logging
 
-from bottle import Bottle, request, run
-
+from flask import Flask, request
 from broker.scheduling import Scheduler
 
+app = Flask(__name__)  # pylint: disable=C0103
 
+
+@app.route("/<user>", methods=["POST"])
 def append_job(user):
     """Receives a POST request to append a new job to the scheduler"""
     logging.info("Received POST request from %s", user)
-    schedule.add_job(user, request.POST)
+    schedule.add_job(user, request.form)
+    return "Added job to schedule\n"
 
 
-def setup_rooting(app):
-    """Configures explicit rooting"""
-    app.route("/<user>", "POST", append_job)
+@app.route("/<user>", methods=["GET"])
+def get_job_list(user):
+    """Receives a GET request and returns all the user's jobs"""
+    res = ""
+    jobs = schedule.get_jobs(user)
+    for job in jobs:
+        res += job.to_json()
+        res += "\n"
+    return res
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     schedule = Scheduler("data.db")  # pylint: disable=C0103
-    print(schedule)
-    web_app = Bottle()  # pylint: disable=C0103
-    setup_rooting(web_app)
-    run(web_app, host="localhost", port=8080)
+    app.run()
