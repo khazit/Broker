c4920d7b081dba58126fb2387c70c0170338cfac
Ali Benkassou <a.benkassou1@gmail.com>
2020-04-13 23:15:14 +0200

    Fix linting error

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from broker.scheduling import Scheduler
 logging.basicConfig(level=logging.INFO)
 schedule = Scheduler("data.db")  # pylint: disable=C0103
 app = Flask(__name__)  # pylint: disable=C0103
-cors = CORS(app)
+cors = CORS(app)  # pylint: disable=C0103
 
 @app.route("/jobs/add", methods=["POST"])
 def append_job():
@@ -32,13 +32,12 @@ def get_job_list(user):
     for job in jobs:
         res.append(job.to_dict())
     response = jsonify(res)
-    response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 
-@app.route("/<user>/<job_id>", methods=["DELETE"])
-def remove_job(user, job_id):
+@app.route("/jobs/remove/<job_id>", methods=["DELETE"])
+def remove_job(job_id):
     """Handles a DELETE request to remove a job from the schedule"""
-    logging.info("Received DELETE request from %s", user)
+    logging.info("Received DELETE request to remove  %s", job_id)
     schedule.remove_job(int(job_id))
     return "Removed job from schedule\n"
