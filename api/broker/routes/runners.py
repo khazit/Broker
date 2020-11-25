506b09e86d18e93d395589b254068133657038fb
Ali Benkassou <a.benkassou1@gmail.com>
2020-11-25 11:52:52 +0100

    Better logging

--- api/broker/routes/runners.py
+++ api/broker/routes/runners.py
@@ -8,30 +8,39 @@ from flask import request, jsonify
 from broker.core.utils import is_status_valid
 
 
+logger = logging.getLogger(__name__)
+
+
 @app.route("/runners/available-job", methods=["GET"])
 def get_available_job():
     """Gets an available job"""
-    logging.info("GET - r %s", None)
+    logger.info("REQUEST: Runner asked for an available job")
     job = app.schedule.get_next()
     if job is None:
+        logger.info("RESPONSE: No more jobs available.")
         return jsonify(None), 204
-    return jsonify(app.schedule.get_next().to_dict()), 200
+    logger.info("RESPONSE: Job %i sent to runner", job.identifier)
+    return jsonify(job.to_dict()), 200
 
 
 @app.route("/runners/update-job", methods=["PUT"])
 def update_job_status():
     """Updates a job's status"""
     payload = request.json
-    logging.info("POST - r %s - id %s", None, payload["identifier"])
+    logger.info(
+        "REQUEST: Runner want to update job %i to status %s",
+        payload["identifier"], payload["status"])
     if not is_status_valid(payload["status"]):
+        logger.error("Status is not valid")
         return jsonify(
             error=f"Invalid value for status {payload['status']}"
         ), 400
     try:
         app.schedule.update_job_status(payload["identifier"], payload["status"])
+        logger.info("RESPONSE: Updated job status")
         return jsonify(), 204
     except IndexError as err:
-        logging.error(err)
+        logger.error(err)
         return jsonify(
             error=f"Resource Job #{payload['identifier']} not found"
         ), 404
